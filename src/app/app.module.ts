6893e0e653eba729cb1d0e6c7a6fd454a0c8cfa1
Luis Henrique Ries <prof.ries@gmail.com>
2024-09-19 21:36:26 -0300

    Atividade pipe - ex1 e ex2

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
+import { FormProdutosComponent } from './form-produtos/form-produtos.component';
+import { FormsModule } from '@angular/forms';
+import { CardProdutoComponent } from './card-produto/card-produto.component';
+import { ListCardProdutosComponent } from './list-card-produtos/list-card-produtos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabelaProdutosComponent,
+    FormProdutosComponent,
+    CardProdutoComponent,
+    ListCardProdutosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
